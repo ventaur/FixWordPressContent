991b122c2828d358a654359acefb3ec482da190a
Matt Sollars <ventaur@gmail.com>
2020-02-24 22:02:26 -0500

    Refactor so the JSON output indent size is pulled from the config file.

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function fix(configPath, exportPath, fixedExportPath) {
     const data = readJsonFile(exportPath);
     
     const fixedData = fixAll(config, data);
-    writeJsonFile(fixedExportPath, fixedData);
+    writeJsonFile(fixedExportPath, fixedData, config.jsonOutputIndentSize);
 }
 
 
